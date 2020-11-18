f462a5734b6f7de84a82763369eccf964fbc363b
Flavien <flavien.murail@gmail.com>
2020-11-18 16:29:04 +0100

    Made the model tests with Testcase

--- lengow/orders/tests/tests_models.py
+++ lengow/orders/tests/tests_models.py
@@ -18,7 +18,31 @@ class TestModels(TestCase):
         """Create a temp order to perform tests"""
         Order.objects.create(
             marketplace="Cdiscount",
-            customer="User 1",
-            payment_date="2020-18-11",
+            customer="User",
+            payment_date="2020-11-18",
             order_amount="56.13"
         )
+
+    def test_order_max_lenght(self):
+        """To check the order fields max_lenght"""
+        data = Order.objects.get(id=1)
+        max_length = data._meta.get_field('marketplace').max_length
+        self.assertEquals(max_length, 100)
+
+    def test_order_verbose_name(self):
+        """To check the order fields verbose names"""
+        data = Order.objects.get(id=1)
+        verbose_name = data._meta.get_field('customer').verbose_name
+        self.assertEquals(verbose_name, "Prénom / Nom")
+
+    def test_order_max_digits(self):
+        """To check the order amount max digits"""
+        data = Order.objects.get(id=1)
+        max_digits = data._meta.get_field('order_amount').max_digits
+        self.assertEquals(max_digits, 5)
+
+    def test_order_decimals(self):
+        """To check the order amount decimals"""
+        data = Order.objects.get(id=1)
+        decimals = data._meta.get_field('order_amount').decimal_places
+        self.assertEquals(decimals, 2)
