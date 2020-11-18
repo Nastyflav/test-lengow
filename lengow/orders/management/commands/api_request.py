bd0a9659a62fb2922e0c539cef6d3cd8c0ad0d16
Flavien <flavien.murail@gmail.com>
2020-11-18 18:40:20 +0100

    Added the file to request the api

---
+++ lengow/orders/management/commands/api_request.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python3
+# coding: utf-8
+
+"""
+Author: [Nastyflav](https://github.com/Nastyflav) 2020-18-11
+Licence: `GNU GPL v3` GNU GPL v3: http://www.gnu.org/licenses/
+
+"""
+
+from orders.models import Order
---
+++ lengow/orders/management/commands/db_init.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python3
+# coding: utf-8
+
+"""
+Author: [Nastyflav](https://github.com/Nastyflav) 2020-18-11
+Licence: `GNU GPL v3` GNU GPL v3: http://www.gnu.org/licenses/
+
+"""
+
+from django.core.management.base import BaseCommand
+
