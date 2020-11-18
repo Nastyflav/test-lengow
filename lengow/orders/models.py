cb99758105d56178850720e25f0b9eb78164525a
Flavien <flavien.murail@gmail.com>
2020-11-18 16:07:42 +0100

    Created the order model and made the migration

--- lengow/orders/models.py
+++ lengow/orders/models.py
@@ -7,3 +7,23 @@ Licence: `GNU GPL v3` GNU GPL v3: http://www.gnu.org/licenses/
 
 """
 
+from django.db import models
+
+
+class Order(models.Model):
+    """Order model, restricted to a few fields"""
+    marketplace = models.CharField(
+        max_length=100, verbose_name='Marketplace', null=True
+    )
+    customer = models.CharField(
+        max_length=200, verbose_name='Prénom / Nom', null=True
+    )
+    payment_date = models.DateField(
+        verbose_name='Date du paiement', null=True
+    )
+    order_amount = models.DecimalField(
+        max_digits=5, decimal_places=2, verbose_name='Montant en euros', null=True
+    )
+
+    class Meta:
+        verbose_name= 'Commande'
--- lengow/orders/tests/tests_models.py
+++ lengow/orders/tests/tests_models.py
@@ -6,3 +6,19 @@ Author: [Nastyflav](https://github.com/Nastyflav) 2020-18-11
 Licence: `GNU GPL v3` GNU GPL v3: http://www.gnu.org/licenses/
 
 """
+
+from django.test import TestCase
+
+from orders.models import Order
+
+
+class TestModels(TestCase):
+    """To test the orders app models"""
+    def setUpTestData():
+        """Create a temp order to perform tests"""
+        Order.objects.create(
+            marketplace="Cdiscount",
+            customer="User 1",
+            payment_date="2020-18-11",
+            order_amount="56.13"
+        )
