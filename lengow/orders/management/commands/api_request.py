115219ab8d2dd194e495859eea04cfc612004956
Flavien <flavien.murail@gmail.com>
2020-11-19 12:19:30 +0100

    Created the command script to populate the database with the API datas

--- lengow/orders/management/commands/api_request.py
+++ lengow/orders/management/commands/api_request.py
@@ -7,4 +7,28 @@ Licence: `GNU GPL v3` GNU GPL v3: http://www.gnu.org/licenses/
 
 """
 
+import xml.etree.ElementTree as ElmTree
+from urllib.request import urlopen
+
 from orders.models import Order
+
+
+class APIRequest():
+    """Get all the needed datas from the API"""
+    def add_datas(self):
+        """To parse the xml url and get the wanted datas"""
+        with urlopen('http://test.lengow.io/orders-test.xml') as url:
+            tree = ElmTree.parse(url)
+            root = tree.getroot()
+            for data in root[1].findall('order'):
+                marketplace = data.find('marketplace').text
+                customer = data.find('billing_lastname').text
+                order_purchase_date = data.find('order_purchase_date').text
+                order_amount = data.find('order_amount').text
+
+                order = Order.objects.create(
+                    marketplace=marketplace,
+                    customer=customer,
+                    order_purchase_date = order_purchase_date,
+                    order_amount = order_amount
+                )
--- lengow/orders/management/commands/db_init.py
+++ lengow/orders/management/commands/db_init.py
@@ -9,3 +9,13 @@ Licence: `GNU GPL v3` GNU GPL v3: http://www.gnu.org/licenses/
 
 from django.core.management.base import BaseCommand
 
+from .api_request import APIRequest
+
+
+class Command(BaseCommand):
+    """Command class for custom django commands"""
+    help='Load datas from the xml API to our local database'
+
+    def handle(self, *args, **options):
+        self.db = APIRequest()
+        self.db.add_datas()
